506407c1c8cbfdff3884b2bae96e838b41c28caa
DeSheger <pgsteam@interia.pl>
2023-07-29 22:19:25 +0200

    Add data to new branch

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+        {}
+
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<User> Users { get;}
+        public DbSet<Group> Groups{ get; set; }
+        public DbSet<Company> Companies{ get; set; }
+    }
+}
