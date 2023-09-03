c05930c0446270e6e803b10c73f5d4def38e4c3b
DeSheger <pgsteam@interia.pl>
2023-09-03 23:31:38 +0200

    CompanyMembers in client has been add

---
+++ client-app/src/components/Company/CompanyContent/CompanyMembers.tsx
@@ -0,0 +1,15 @@
+
+const Member = () => {
+    return
+}
+
+const CompanyMembers = ({companyMembers}:any) => {
+
+    return (
+        <section className="companyMembers">
+            
+        </section>
+    )
+}
+
+export default CompanyMembers;
