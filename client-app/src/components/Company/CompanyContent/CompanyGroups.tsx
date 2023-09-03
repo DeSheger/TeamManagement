481ff39271de846542d9f6ee0951544c7919c38f
DeSheger <pgsteam@interia.pl>
2023-09-03 23:32:29 +0200

    CompanyGroups in client has been add

---
+++ client-app/src/components/Company/CompanyContent/CompanyGroups.tsx
@@ -0,0 +1,15 @@
+
+const Group = () => {
+    return
+}
+
+const CompanyGroups = ({companyMembers}:any) => {
+
+    return (
+        <section className="companyGroups">
+            
+        </section>
+    )
+}
+
+export default CompanyGroups;
