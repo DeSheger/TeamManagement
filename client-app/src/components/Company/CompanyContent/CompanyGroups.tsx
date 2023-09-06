eedbd5190b6843aa38ae4e6bee4375cc386355e5
DeSheger <pgsteam@interia.pl>
2023-09-06 19:28:39 +0200

    Company Groups finish in client

--- client-app/src/components/Company/CompanyContent/CompanyGroups.tsx
+++ client-app/src/components/Company/CompanyContent/CompanyGroups.tsx
@@ -1,53 +1,48 @@
 import { useEffect, useState } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Card, Container, Row } from "react-bootstrap";
 import getCompanyGroups from "../../../services/getCompanyGroups";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../app/store";
 
-const Group = ({group}:any) => {
+const Group = ({ group }: any) => {
     const groupName = group?.name;
-    const groupLeader = group?.leader?.name;
+    const groupLeader = group?.leader?.name + " " + group?.leader?.surrname;
     const groupDescription = group?.description;
-    const groupMembers = group?.members.map((member:any) => member.name+" ")
+    const groupMembers = group?.members.map((member: any) => member.name + " " + member.surrname+ ", ")
 
-    return(
-        <Container>
-            <Row>
-                <h6>Name</h6>
-                {groupName}
-            </Row>
-            <Row>
-                <h6>Leader</h6>
-                {groupLeader}
-            </Row>
-            <Row>
-                <h6>Description</h6>
-                {groupDescription}
-            </Row>
-            <Row>
-                <h6>Members</h6>
-                {groupMembers}
-            </Row>
-
-        </Container>
+    return (
+        <Card bg="primary" text="light" style={{ width: '18rem', margin:"4px"}}>
+            <Card.Body>
+                <Card.Title>{groupName}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{groupDescription}</Card.Subtitle>
+                <Card.Text>
+                    <h6>Leader</h6>
+                    {groupLeader}
+                </Card.Text>
+                <Card.Text>
+                    <h6>Members</h6>
+                    {groupMembers}
+                </Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
 
 const CompanyGroups = ({ company }: any) => {
     const [companyGroups, seCompanyGroups] = useState([]);
-    const session = useSelector((state:RootState) => state.session)
+    const session = useSelector((state: RootState) => state.session)
 
     useEffect(() => {
         getCompanyGroups(company.id, session)
             .then((val) => seCompanyGroups(val))
             .catch((err) => console.log(err))
-    },[company])
+    }, [company])
 
     return (
         <Container>
             <Row>
-               {companyGroups?.map((group:Object) => <Group group={group}/>)}
-            
+                {companyGroups.map((group: Object) => <Group group={group} />)}
+
             </Row>
         </Container>
     )
