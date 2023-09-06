f360b703527e4ecdb68f1c23ebc826cd6592b1a7
DeSheger <pgsteam@interia.pl>
2023-09-06 19:13:14 +0200

    Company Groups add in client

--- client-app/src/components/Company/CompanyContent/CompanyGroups.tsx
+++ client-app/src/components/Company/CompanyContent/CompanyGroups.tsx
@@ -4,28 +4,49 @@ import getCompanyGroups from "../../../services/getCompanyGroups";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../app/store";
 
+const Group = ({group}:any) => {
+    const groupName = group?.name;
+    const groupLeader = group?.leader?.name;
+    const groupDescription = group?.description;
+    const groupMembers = group?.members.map((member:any) => member.name+" ")
+
+    return(
+        <Container>
+            <Row>
+                <h6>Name</h6>
+                {groupName}
+            </Row>
+            <Row>
+                <h6>Leader</h6>
+                {groupLeader}
+            </Row>
+            <Row>
+                <h6>Description</h6>
+                {groupDescription}
+            </Row>
+            <Row>
+                <h6>Members</h6>
+                {groupMembers}
+            </Row>
+
+        </Container>
+    )
+}
+
 const CompanyGroups = ({ company }: any) => {
     const [companyGroups, seCompanyGroups] = useState([]);
     const session = useSelector((state:RootState) => state.session)
 
     useEffect(() => {
         getCompanyGroups(company.id, session)
-            .then((val) => console.log(val))
+            .then((val) => seCompanyGroups(val))
             .catch((err) => console.log(err))
-    },[])
+    },[company])
 
     return (
         <Container>
             <Row>
-                <h4>Leader</h4>
-            </Row>
-            <Row>
-            
-            </Row>
-            <Row>
-                <h4>Description</h4>
-            </Row>
-            <Row>
+               {companyGroups?.map((group:Object) => <Group group={group}/>)}
             
             </Row>
         </Container>
