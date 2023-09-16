60141a7e921ffda6c4119a2b08c1dd47b56bf7fe
DeSheger <pgsteam@interia.pl>
2023-09-16 18:35:34 +0200

    add client in vue

---
+++ client-vue/src/pages/main.vue
@@ -0,0 +1,14 @@
+<script setup>
+    useHead({
+        title: "Main"
+    });
+</script>
+
+<template>
+    <div>Hello main</div>
+    <router-link :to="{ name: 'home' }">Home</router-link>
+</template>
+
+<route lang="yaml">
+    name: main
+</route>
