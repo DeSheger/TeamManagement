a076a88be7df0af2ebabcc0ceefc463d76436290
DeSheger <pgsteam@interia.pl>
2023-09-19 16:32:35 +0200

    Finish menu bar in vue

--- client-vue/components.d.ts
+++ client-vue/components.d.ts
@@ -7,10 +7,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    AppHeader: typeof import('./src/components/AppHeader.vue')['default']
-    'Icon:bx:bxMoon': typeof import('~icons/bx/bx-moon')['default']
-    'Icon:bx:bxsMoon': typeof import('~icons/bx/bxs-moon')['default']
-    'IconAkarIcons:githubFill': typeof import('~icons/akar-icons/github-fill')['default']
     Menu: typeof import('./src/components/menu/Menu.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
