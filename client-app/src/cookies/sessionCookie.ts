b23e5a4208a747322710fd5c6558e0c9ba217d97
DeSheger <groszpiotr.mitrox@gmail.com>
2023-12-27 11:59:39 +0100

    (gitignore) delete unnecsesery files

--- client-app/src/cookies/sessionCookie.ts
+++
@@ -1,9 +0,0 @@
-import { SessionState } from "../features/session/sessionSlice";
-
-function sessionCookie(user: SessionState) {
-    
-    document.cookie = "user=" + JSON.stringify(user);
-
-}
-
-export default sessionCookie;
--- client-app/src/tests/reducers/sessionSlice.test.ts
+++
@@ -1,7 +0,0 @@
-import { session } from '../../features/session/sessionSlice';
-
-test('reducers', () => {
-  let state;
-  state = session({email:'piotr@test.com',name:'Piotr',surrname:'Kowalski',token:"null"});
-  expect(state).toEqual({email:'piotr@test.com',name:'Piotr',surrname:'Kowalski',token:'null'});
-});
